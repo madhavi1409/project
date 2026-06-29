0e26f0b71fd667b63c70740c216b5122614d68cd
madhavi1409 <its.madhavi1409@gmail.com>
2026-06-29 10:28:31 +0530

    Added .gitignore

---
+++ models/Post.js
@@ -0,0 +1,18 @@
+const mongooose = require("mongoose");
+const postSchema = new mongoose.Schema({
+    title:{
+      type:String,
+      required:true      
+    },
+    description:{
+      type:String,
+      required:true
+    },
+    user:{
+      type:mongoose.Schema.Types.ObjectId,
+      ref:"User"
+    }
+},{
+  timestamps:true
+});
+module.exports = mongoose.model("Post",postSchema);
